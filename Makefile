489d531f6e033a255912791236d58b67fed06240
John Freeman <jfreeman08@gmail.com>
2018-09-26 23:22:44 -0500

    Asynchronous catch

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test :
 	pytest
-	mypy --module multi_error
+	mypy --module multi_error --ignore-missing-imports
 	flake8
 	python example.py
--- multi_error.py
+++ multi_error.py
@@ -1,5 +1,7 @@
+from async_generator import asynccontextmanager
+from contextlib import contextmanager
 import io
-from typing import Callable, cast, List, Type, Tuple, Union
+from typing import Any, Callable, cast, List, Optional, Type, Tuple, Union
 
 
 Predicate = Callable[[BaseException], bool]
@@ -14,7 +16,7 @@ PredicateLike = Union[
     Predicate,
 ]
 
-SplitError = Tuple[Union['MultiError', None], Union['MultiError', None]]
+SplitError = Tuple[Optional[BaseException], Optional[BaseException]]
 
 
 class MultiError(Exception):
@@ -32,9 +34,39 @@ class MultiError(Exception):
     def __str__(self):
         return _tree(self).getvalue()
 
+    def _copy_with(self, *args, **kwargs):
+        """Copy this exception but with different arguments.
+
+        Special attributes of exceptions (:attribute:`__context__`,
+        :attribute:`__cause__`, and :attribute:`__traceback__`) are copied
+        over to the new exception from this exception.
+
+        Parameters
+        ----------
+        errors :
+            A list of exceptions.
+
+        Returns
+        -------
+        MultiError
+            A copy of this exception, but with different arguments.
+        """
+        copy = self.__class__(*args, **kwargs)
+        copy.__context__ = self.__context__
+        copy.__cause__ = self.__cause__
+        copy.__traceback__ = self.__traceback__
+        return copy
+
     @staticmethod
-    def split(predicate: PredicateLike, error: 'MultiError') -> SplitError:
+    def split(predicate: PredicateLike, error: BaseException) -> SplitError:
+        # Normalize our parameters.
         predicate = _predicate(predicate)
+
+        # Handle exceptions that are not :class:`MultiError`.
+        if not isinstance(error, MultiError):
+            return (error, None) if predicate(error) else (None, error)
+
+        # Yeses and noes.
         ys = cast(List[BaseException], [])
         ns = cast(List[BaseException], [])
         for child in error.errors:
@@ -48,10 +80,41 @@ class MultiError(Exception):
                 ys.append(child)
             else:
                 ns.append(child)
-        # TODO: Copy over traceback and other context.
-        match = MultiError(ys) if ys else None
-        rest = MultiError(ns) if ns else None
+
+        match = error._copy_with(ys) if ys else None
+        rest = error._copy_with(ns) if ns else None
         return (match, rest)
+
+    @staticmethod
+    @contextmanager
+    def catch(
+            predicate: PredicateLike, handler: Callable[['MultiError'], Any],
+    ):
+        try:
+            yield
+        except BaseException as error:
+            with _catching(predicate, error) as caught:
+                handler(cast(MultiError, caught))
+
+    @staticmethod
+    @asynccontextmanager
+    async def acatch(
+            predicate: PredicateLike, handler: Callable[['MultiError'], Any],
+    ):
+        try:
+            yield
+        except BaseException as error:
+            with _catching(predicate, error) as caught:
+                await handler(cast(MultiError, caught))
+
+
+@contextmanager
+def _catching(predicate: PredicateLike, error: BaseException):
+    caught, rest = MultiError.split(predicate, error)
+    if caught is not None:
+        yield caught
+    if rest is not None:
+        raise rest
 
 
 def _predicate(input: PredicateLike) -> Predicate:
